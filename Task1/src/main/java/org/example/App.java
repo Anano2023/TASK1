19c7ea6035c68aea4bc2ab4b2980f8d1c3ab1f6d
Anano <141433300+Anano2023@users.noreply.github.com>
2024-04-24 13:23:23 +0400

    Add files via upload

---
+++ Task1/src/main/java/org/example/App.java
@@ -0,0 +1,45 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class App
+{
+    private WebDriver driver;
+    private final String LINK = "https://pastebin.com/";
+    PasteBinPage mainPage;
+    PasteListPage myPastes;
+
+    @BeforeClass
+    public void setup() {
+
+        WebDriverManager.edgedriver().setup();
+        EdgeOptions options = new EdgeOptions();
+       // driver.manage().window().maximize();
+       // options.addArguments("--start-maximized");
+
+        driver = new EdgeDriver(options);
+        driver.get(LINK);
+
+        mainPage = new PasteBinPage(driver);
+        myPastes = new PasteListPage(driver);
+    }
+
+
+
+    @Test(priority = 4)
+    public void createNewPaste() {
+        mainPage.writeTextInNewPasteField("Hello from WebDriver");
+        mainPage.selectPasteExpirationDropdown();
+        mainPage.writeTextInPasteTitle("helloweb");
+        mainPage.clickOnCreateNewPaste();
+        Assert.assertTrue(myPastes.getNoteMessage().contains("NOTE: Your guest paste has been posted"));
+        driver.quit();
+    }
+}
